ae6a06a3b33696586b2b716a96ccc4044af2abf5
Lucas Gabriel <105575164+lucasbernardodev@users.noreply.github.com>
2022-09-01 11:04:59 -0300

    Adição aulas 10 e 11

---
+++ Aulas/DIR_E_HELP.py
@@ -0,0 +1,21 @@
+# 11. Dir e Help
+
+# Utilitários Python para auxiliar na programação.
+
+# Dir -> Apresenta todos os atributos/propriedades e funções/métodos disponíveis
+# para determinado tipo de dado ou variável.
+
+# Exemplo:
+
+# Quero saber o que posso fazer com uma string
+
+dir('umastring')
+
+# Help -> Apresenta a documentação/como utilizar os atributos/propriedades e funções/métodos
+# disponíveis para determinado tipo de dado ou variável.
+
+# Exemplo:
+
+# Quero saber o que faço com alguma função
+
+help('umastring'.upper)
