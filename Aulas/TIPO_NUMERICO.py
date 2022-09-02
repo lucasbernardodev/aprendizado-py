d2dce92a803e4892da0f27bbe8bf698f34ba4f24
Lucas Gabriel <105575164+lucasbernardodev@users.noreply.github.com>
2022-09-02 09:37:07 -0300

    Adição aula 15

---
+++ Aulas/TIPO_NUMERICO.py
@@ -0,0 +1,56 @@
+# 15. O tipo númerico
+
+# Vendo o tipo de um dado
+num = 42
+print('Tipo:',type(num))
+
+
+num2 = 42.0
+print('Tipo:',type(num2))
+
+
+num3 = '42'
+print('Tipo:',type(num3))
+
+# Operações com números
+# Soma
+print(2 + 2)
+
+# Subtração
+print(2 - 2)
+
+# Multiplicação
+print(2 * 2)
+
+# Divisão
+print(2 / 2)
+
+# Potenciação
+print(2 ** 2)
+
+# Resultado inteiro da divisão
+print(2 // 2)
+
+# Resto da divisão
+print(2 % 2)
+
+# Resto da divisão
+print(3 % 2)
+
+# Soma da função
+# Em vez de fazer n + 1, podemos fazer n += 1
+n = 1
+n += 1
+# n -= 1
+# n *= 1
+# n /= 1
+# n **= 1
+# n //= 1
+# n %= 1
+
+print(n)
+
+# n / d = :
+n = int(input('Digite um valor para divisão: ')) # Inserindo valor
+d = int(input('Digite um valor para ser o divisor: ')) # Inserindo divisor
+print('O resultado da divisão é: ',int(n / d)) # Divisão
